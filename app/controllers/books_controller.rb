959f0b896f70c19d2d0265b40d37308c17df114e
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-11-03 12:01:47 +0000

    [2-12,day3]課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,37 @@
 class BooksController < ApplicationController
   def index
     @books=Book.all
-  end
-
-  def new
-    @books=Book.new
+    @book=Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to 'books/107986'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path
+  end
+
+
 
     private
   # ストロングパラメータ
