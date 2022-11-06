8dbd73241165a99f67f81751ddd9fe2f1bae4f31
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-11-06 08:19:39 +0000

    [2-12,day4]課題4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
+   if @book.save
+    flash[:notice]="successfully"
     redirect_to book_path(@book.id)
-
+   else
+     @books=Book.all
+     render:index
+   end
   end
 
   def show
@@ -20,21 +24,27 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="successfully"
+      redirect_to book_path(@book)
+    else
+      render:edit
+    end
+
   end
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice]="successfully"
     redirect_to books_path
   end
 
 
 
     private
-  # ストロングパラメータ
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
